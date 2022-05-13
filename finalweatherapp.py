928b4af7b4e6f649ecd4cb39f1cf1c94041785cd
akashdeep24 <97055358+akashdeep24@users.noreply.github.com>
2022-05-13 14:36:03 +0530

    Update finalweatherapp.py

--- finalweatherapp.py
+++ finalweatherapp.py
@@ -29,11 +29,13 @@ def takecity():
 def delete_city():
     City.delete("0","end")
 
+def counter(x):
+    my_label.config(text = x)
 
 def sendcity(func):
     argue = func()
+    counter(argue)
     text = weatherreport(argue)
-    City_name.insert(END, argue)
     Temprature.insert(0, text[0])
     Feelslike.insert(0, text[1])
     max_temp.insert(0, text[2])
@@ -42,7 +44,6 @@ def sendcity(func):
     Pressure.insert(0, text[5])
     
 def delete_all_text():
-    City_name.delete("0","end")
     Temprature.delete("0","end")
     Feelslike.delete("0","end")
     max_temp.delete("0","end")
@@ -50,23 +51,22 @@ def delete_all_text():
     Humidity.delete("0","end")
     Pressure.delete("0","end")
 
-
-
 window = Tk()
+window.geometry("300x250")
 window.title("weather update")
-window.geometry("300x200")
 Label(window, text="Enter City", bg="sky blue").grid(row=0)
-Label(window, text="City Name", bg="sky blue").grid(row=1)
+my_label=Label(window, text="",font=("Comic Sans MS", 15,), bg="sky blue")
+my_label.grid(row=1, column=1)
 Label(window, text="Temprature", bg="sky blue").grid(row=2)
 Label(window, text="Feels like", bg="sky blue").grid(row=3)
 Label(window, text="Max. temprature", bg="sky blue").grid(row=4)
 Label(window, text="Min. temprature", bg="sky blue").grid(row=5)
 Label(window, text="Humidity", bg="sky blue").grid(row=6)
 Label(window, text="Pressure", bg="sky blue").grid(row=7)
-button = Button(window, text="Enter",bg="MistyRose", width=10, command=lambda: [delete_all_text(),sendcity(takecity), delete_city()])
+btn=button = Button(window, text="Enter",bg="MistyRose", width=10,
+ command=lambda: [delete_all_text(),sendcity(takecity), delete_city()])
 button.grid(row=9, column=1)
 City = Entry(window)
-City_name= Entry(window)
 Temprature = Entry(window)
 Feelslike = Entry(window)
 max_temp = Entry(window)
@@ -74,13 +74,11 @@ min_temp = Entry(window)
 Humidity = Entry(window)
 Pressure = Entry(window)
 City.grid(row=0, column=1)
-City_name.grid(row=1, column=1)
 Temprature.grid(row=2, column=1)
 Feelslike.grid(row=3, column=1)
 max_temp.grid(row=4, column=1)
 min_temp.grid(row=5, column=1)
 Humidity.grid(row=6, column=1)
 Pressure.grid(row=7, column=1)
-
 window.configure(bg='sky blue')
 mainloop()
